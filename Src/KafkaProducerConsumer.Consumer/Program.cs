af5dcd8f47b01f6c2d677827e4a4d570bbc0b062
Fernando José <fernandogjose@gmail.com>
2021-04-04 19:00:33 -0300

    primeiro commit

---
+++ Src/KafkaProducerConsumer.Consumer/Program.cs
@@ -0,0 +1,41 @@
+﻿using Confluent.Kafka;
+using System;
+using System.Threading;
+
+namespace KafkaProducerConsumer.Consumer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Consumer 1");
+
+            // Consumer Config
+            var consumerConfig = new ConsumerConfig
+            {
+                GroupId = "test-consumer-group",
+                BootstrapServers = "localhost:9092",
+                AutoOffsetReset = AutoOffsetReset.Earliest
+            };
+
+            using (var c = new ConsumerBuilder<string, string>(consumerConfig).Build())
+            {
+                c.Subscribe("fila_pedido");
+                var cts = new CancellationTokenSource();
+
+                try
+                {
+                    while (true)
+                    {
+                        var message = c.Consume(cts.Token);
+                        Console.WriteLine($"Key: {message.Message.Key}, Message: {message.Message.Value} recebida de {message.TopicPartitionOffset}");
+                    }
+                }
+                catch (OperationCanceledException operationCanceledException)
+                {
+                    c.Close();
+                }
+            }
+        }
+    }
+}
---
+++ Src/KafkaProducerConsumer.Producer/Program.cs
@@ -0,0 +1,47 @@
+﻿using Confluent.Kafka;
+using System;
+
+namespace KafkaProducerConsumer.Producer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Producer");
+
+            var readKeyUser = "1";
+            while (readKeyUser == "1")
+            {
+                for (int i = 0; i < 50; i++)
+                {
+                    var SendMessageByKafkaResponse = SendMessageByKafka($"minha mensagem {i}");
+                    Console.WriteLine($"Mensagem enviada com sucesso - {SendMessageByKafkaResponse}");
+                }
+
+                Console.WriteLine("Rodar novamente");
+                Console.WriteLine("1 = sim");
+                Console.WriteLine("2 = não");
+                readKeyUser = Console.ReadLine();
+            }
+        }
+
+        private static string SendMessageByKafka(string message)
+        {
+            var config = new ProducerConfig { BootstrapServers = "localhost:9092" };
+            using (var producer = new ProducerBuilder<string, string>(config).Build())
+            {
+                try
+                {
+                    var sendResult = producer.ProduceAsync("fila_pedido", new Message<string, string> { Key = message, Value = message }).GetAwaiter().GetResult();
+                    return $"Mensagem '{sendResult.Value}' de '{sendResult.TopicPartitionOffset}'";
+                }
+                catch (ProduceException<Null, string> e)
+                {
+                    Console.WriteLine($"Delivery failed: {e.Error.Reason}");
+                }
+            }
+
+            return string.Empty;
+        }
+    }
+}
